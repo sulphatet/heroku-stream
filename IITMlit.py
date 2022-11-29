811965da56bfb2938d08672484b58a4ddf57a37c
Syed Affan D <73064995+sulphatet@users.noreply.github.com>
2022-11-29 15:01:46 +0530

    Update IITMlit.py

--- IITMlit.py
+++ IITMlit.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 st.title('IITM Graded Assignment')
 
-#choice1 = st.number_input('Enter First number')
-#choice2 = st.number_input('Enter Second number')
-#choice3 = st.number_input('Enter Third number')
+choice1 = st.number_input('Enter First number')
+choice2 = st.number_input('Enter Second number')
+choice3 = st.number_input('Enter Third number')
 
-#string = f'Maximum value is {max(choice1,choice2,choice3)}'
+string = f'Maximum value is {max(choice1,choice2,choice3)}'
 
-#st.write(string)
+st.write(string)
